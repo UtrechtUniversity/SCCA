88b91e9efc34692939915714725fef17a6061bde
Kees van Eijden <k.vaneijden@gmail.com>
2020-03-25 15:55:14 +0100

    adds in compute_symmetric r_sums[r_sums==0] = 1

--- R/compute_symmetric.R
+++ R/compute_symmetric.R
@@ -1,7 +1,12 @@
-#' Compute Symmetric Matrix of a bipartite adjacency matrix
+#' Compute Eigenvalues and Eigenvectors of Large and Sparse Incidence Matrix
 #'
-#' @param matrix A numeric matrix
-#' @param decomp_axis Character string with value 'row' or 'column'.
+#' See <<link>>
+#'
+#' @param matrix An incidence matrix. The rows are the observations and
+#'   the columns are the
+#' @param decomp_axis Character string with value 'row' or 'column' indicating the axis along
+#'   which the decomposition will initially take place. At the end the decomposition will be transformed
+#'   to the row axis (= observations)
 #'
 
 compute_symmetric <- function(matrix, decomp_axis) {
@@ -11,67 +16,66 @@ compute_symmetric <- function(matrix, decomp_axis) {
     stop('Unknown decomposition axis')
   }
 
-  # remove disconnected columns
-  #
-  n_disconnect <- sum(colSums(matrix) == 0)
-  # if (n_disconnect > 0) {
-  #   warning('Disconnected columns: ', n_disconnect)
-  # }
-  matrix <- matrix[ , colSums(matrix) != 0, drop = FALSE]
-
-  # A n*m
-  # D_c = colsum A    m*m
-  # D_r = rowsum A    n*n
-  # S_r = A D_c^{-1} A^T n*m X m*m X m*n --> n*n
-  # return S
-
-  matrix_a <- Matrix::Matrix(matrix, sparse = TRUE)
-  #
-  r_sums <- Matrix::rowSums(matrix_a)
-  c_sums <- Matrix::colSums(matrix_a)
-  if (any(r_sums == 0) ) {
-    warning('any row sum equals 0: ', dim(matrix_a)[1], ' - ', dim(matrix_a)[2])
-  }
-  if (any(c_sums == 0)) {
-    warning('any col sum equals 0: ', dim(matrix_a)[1], ' - ', dim(matrix_a)[2])
-  }
+  # Compute symmetric matrix S
+  # Steps:
+  #   A  matrix with observations (n rows) and variables (m columns)
+  #   D_c = colsum A    (m*m)
+  #   D_r = rowsum A    (n*n)
+  #   S_r = A * D_c^{-1} * A^T (n*m X m*m X m*n --> n*n)
+  #   S_c = A^T * D_r^{-1} * A (m*n X n*n X n*m --> m*m)
 
 
-  d_r_inv <- Matrix::solve(Matrix::Diagonal(x = r_sums), sparse = TRUE) # n X n
-  d_c_inv <- Matrix::solve(Matrix::Diagonal(x = c_sums), sparse = TRUE) # m X m
+  matrix_a <- Matrix::Matrix(matrix, sparse = TRUE)     # Use sparse matrix to speed up computations
+  r_sums   <- Matrix::rowSums(matrix_a)
+  c_sums   <- Matrix::colSums(matrix_a)
 
-  cluster_axis = 'rows'
+  # In the proces columns/rows can become disconnected (sum == 0). To prevent division by 0
+   # set the sum to 1
+   #
+  r_sums[r_sums == 0] <- 1
+  c_sums[c_sums == 0] <- 1
 
-    # calculate symmetric matrix S with dimensions n*n (n is number of rows)
+   # Compute inverted diagonal matrices
+   #
+  d_r_inv <- Matrix::Diagonal(x = 1/r_sums) # n X n
+  d_c_inv <- Matrix::Diagonal(x = 1/c_sums) # m X m
+
+    # compute symmetric matrix S
     #
   if (decomp_axis == 'rows') {
-    s <- matrix_a %*% d_c_inv %*% Matrix::t(matrix_a)  # Sr
+    s     <- matrix_a %*% d_c_inv %*% Matrix::t(matrix_a)  # Sr
     d_inv <- d_r_inv
-  }
-
-  if (decomp_axis == 'cols') {
-    s <- Matrix::t(matrix_a) %*% d_r_inv %*% matrix_a # Sc
+  } else {                             # decomp_axis == 'cols'
+    s     <- Matrix::t(matrix_a) %*% d_r_inv %*% matrix_a # Sc
     d_inv <- d_c_inv
   }
 
   # Decompose symmetric matrix S
   #   Shat = D_r^{-1/2}  S D_r^{-1/2}
-  # U, lambdas = eig(Shat)
-  # V = D_r^{-1/2} U
-  # return V,lambdas
+  #   U, lambdas = eig(Shat)
+  #   V = D_r^{-1/2} U          #
+  #   return V,lambdas
 
-  s_hat         <- sqrt(d_inv) %*% s %*% sqrt(d_inv)
-  if (nrow(s_hat) > 25 ) {
-    oldw <- getOption("warn")
+  s_hat <- sqrt(d_inv) %*% s %*% sqrt(d_inv)
+
+   # Eigenvalues and Eigenvectors of symmetric S
+   #
+  if (nrow(s_hat) > 25 ) {                        # Don't compute more than 25 Eigenvalues for performance reasons
+    #oldw <- getOption("warn")
     #options(warn = -1)
     eigen_decomp <- rARPACK::eigs_sym(A = s_hat, k = 25)
-    options(warn = oldw)
+    #options(warn = oldw)
   } else {
     eigen_decomp <- eigen(x = s_hat, symmetric = TRUE)
   }
+
+  # Eigenvalues and Eigenvectors of the original matrix. Along the decomp_axis
+  #
   eigen_vectors <- sqrt(d_inv) %*% eigen_decomp$vectors
   eigen_values  <- eigen_decomp$values
 
+    # Get 'row' Eigenvectors
+    #
   if (decomp_axis == 'cols') {
     eigen_vectors <- d_r_inv %*% matrix_a %*% eigen_vectors
   }
@@ -80,13 +84,17 @@ compute_symmetric <- function(matrix, decomp_axis) {
   eigen_values  <- eigen_sorted$x
   eigen_vectors <- eigen_vectors[ , eigen_sorted$ix]
 
-  # some scaling but why?
+  # due to rounding errors zero's, don't have to be exactly zero. They even can be negative and that
+  # can cause errors in some comming computations. Within a small tolerance values will be reset to 0
   #
   tolerance = 1e-7
   if (any(eigen_values < -tolerance)) {
     stop('Negative Eigenvalues! Something is definitely going wrong!')
   }
   eigen_values[abs(eigen_values) < tolerance] <- 0
+
+  # some scaling but why?
+  #
   eigen_vectors <- eigen_vectors %*% sqrt(Matrix::Diagonal(x=eigen_values))
 
   return(list(vectors = eigen_vectors, values = eigen_values))
